1d037dc36ddd7163c0e61b298c7e3e8423ea5fb4
Createitv <xfy150150@163.com>
2024-11-07 14:06:40 +0800

    init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/custom-provider/theme-provider";
+import "@/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "T3 stripe start",
+  description: "解放你的双手",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+      <Toaster />
+    </html>
+  );
+}
